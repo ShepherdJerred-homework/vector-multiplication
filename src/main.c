e813a48c92254506aa09d8379eef2ce3b4ccb188
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-09-17 22:03:17 -0500

    Begin impl

--- src/main.c
+++ src/main.c
@@ -1,10 +1,66 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const int MAIN_PID = 0;
+
+void getInput(int pid, int* m, int* n, double** vector, double** matrix) {
+    if (pid == MAIN_PID) {
+        scanf("%i", m);
+    }
+
+    if (pid == MAIN_PID) {
+        scanf("%i", n);
+    }
+
+    // TODO broadcast
+
+    if (pid == MAIN_PID) {
+        *vector = calloc(*n, sizeof(double));
+
+        int i;
+        for (i = 0; i < *n; i++) {
+            scanf("%lf", vector[i]);
+        }
+    }
+
+    // TODO broadcast
+
+    if (pid == MAIN_PID) {
+        int numberOfNumbersInMatrix = *n * *m;
+        *matrix = calloc(numberOfNumbersInMatrix, sizeof(double));
+
+        int i;
+        for (i = 0; i < numberOfNumbersInMatrix; i++) {
+            // TODO pointer arithmetic
+        }
+    }
+
+    // TODO broadcast
+}
+
+void calculateColumnsPerProcess(int numberOfProcesses, int n, int* numberOfColumns) {
+    *numberOfColumns = numberOfProcesses / n;
+}
+
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
 
-    int pid = getPid();
-    int numberOfProcesses = getNumberOfProcesses();
+    int pid;
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
-    run(pid, numberOfProcesses);
+    int numberOfProcesses;
+    MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
+
+    int m;
+    int n;
+    double* vector;
+    double* matrix;
+
+    int numberOfColumns;
+
+    getInput(pid, &m, &n, &vector, &matrix);
+    calculateColumnsPerProcess(numberOfProcesses, n, &numberOfColumns);
 
     MPI_Finalize();
 }
