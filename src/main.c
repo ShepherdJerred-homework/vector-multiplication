4ac8b72a8e9470f5a59bfa3ffe2b64750a70a6d0
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-09-18 12:49:03 -0500

    I tried

--- src/main.c
+++ src/main.c
@@ -4,43 +4,100 @@
 
 const int MAIN_PID = 0;
 
-void getInput(int pid, int* m, int* n, double** vector, double** matrix) {
-    if (pid == MAIN_PID) {
-        scanf("%i", m);
-    }
+struct Params {
+    int m;
+    int n;
+};
 
-    if (pid == MAIN_PID) {
-        scanf("%i", n);
-    }
-
-    // TODO broadcast
-
-    if (pid == MAIN_PID) {
-        *vector = calloc(*n, sizeof(double));
-
-        int i;
-        for (i = 0; i < *n; i++) {
-            scanf("%lf", vector[i]);
-        }
-    }
-
-    // TODO broadcast
-
-    if (pid == MAIN_PID) {
-        int numberOfNumbersInMatrix = *n * *m;
-        *matrix = calloc(numberOfNumbersInMatrix, sizeof(double));
-
-        int i;
-        for (i = 0; i < numberOfNumbersInMatrix; i++) {
-            // TODO pointer arithmetic
-        }
-    }
-
-    // TODO broadcast
+// https://stackoverflow.com/questions/10788180/sending-columns-of-a-matrix-using-mpi-scatter
+// https://www.mpich.org/static/docs/v3.2/www3/MPI_Type_vector.html
+void createVectorType(int m, int n, MPI_Datatype *newType) {
+    MPI_Datatype vectorColumnType;
+    MPI_Type_vector(m, 1, n, MPI_DOUBLE, &vectorColumnType);
+    MPI_Type_commit(&vectorColumnType);
+    MPI_Type_create_resized(vectorColumnType, 0, sizeof(double), newType);
+    MPI_Type_commit(newType);
 }
 
-void calculateColumnsPerProcess(int numberOfProcesses, int n, int* numberOfColumns) {
-    *numberOfColumns = numberOfProcesses / n;
+// https://stackoverflow.com/questions/20040663/how-to-create-new-type-in-mpi
+// https://stackoverflow.com/questions/9864510/struct-serialization-in-c-and-transfer-over-mpi
+void createParamsType(MPI_Datatype *newType) {
+    int count = 2;
+    int blocklens[] = {1, 1};
+    MPI_Aint indices[2];
+
+    indices[0] = (MPI_Aint) offsetof(
+    struct Params, m);
+    indices[1] = (MPI_Aint) offsetof(
+    struct Params, n);
+
+    MPI_Datatype mpiTypes[] = {MPI_INT, MPI_INT};
+
+    MPI_Type_create_struct(count, blocklens, indices, mpiTypes, newType);
+    MPI_Type_commit(newType);
+}
+
+void calculateNumberOfNumbersInMatrix(int m, int n, int *numberOfNumbersInMatrix) {
+    *numberOfNumbersInMatrix = m * n;
+}
+
+void getInput(int *m, int *n, double **vector, double **matrix) {
+    scanf("%i", m);
+    scanf("%i", n);
+
+    *vector = calloc((size_t) * m, sizeof(double));
+
+    int i;
+    for (i = 0; i < *m; i++) {
+        scanf("%lf", (*vector + i));
+    }
+
+    int numberOfNumbersInMatrix;
+    calculateNumberOfNumbersInMatrix(*m, *n, &numberOfNumbersInMatrix);
+
+    *matrix = calloc((size_t) numberOfNumbersInMatrix, sizeof(double));
+
+    for (i = 0; i < numberOfNumbersInMatrix; i++) {
+        scanf("%lf", (*matrix + i));
+    }
+}
+
+void calculateColumnsPerProcess(int numberOfProcesses, int n, int *numberOfColumns) {
+    *numberOfColumns = n / numberOfProcesses;
+}
+
+void calculateNumberOfNumbersPerProcess(int m, int numberOfColumnsPerProcess, int *numberOfNumbersPerProcess) {
+    *numberOfNumbersPerProcess = m * numberOfColumnsPerProcess;
+}
+
+void broadcastParams(MPI_Datatype type, int *m, int *n) {
+    struct Params params;
+    params.m = *m;
+    params.n = *n;
+    MPI_Bcast(&params, 1, type, MAIN_PID, MPI_COMM_WORLD);
+    *m = params.m;
+    *n = params.n;
+}
+
+void broadcastVector(int pid, int m, double **vector) {
+    if (pid != MAIN_PID) {
+        *vector = calloc((size_t) m, sizeof(double));
+    }
+    MPI_Bcast(*vector, m, MPI_DOUBLE, MAIN_PID, MPI_COMM_WORLD);
+}
+
+void
+broadcastMatrix(MPI_Datatype vectorType, int numberOfNumbersPerProcess, int numberOfColumnsPerProcess, double *matrix,
+                double **myMatrix) {
+//    printf("nnpp: %i, ncpp: %i\n", numberOfNumbersPerProcess, numberOfColumnsPerProcess);
+    *myMatrix = calloc((size_t) numberOfNumbersPerProcess, sizeof(double));
+    MPI_Scatter(matrix, numberOfColumnsPerProcess, vectorType, *myMatrix, numberOfNumbersPerProcess, MPI_DOUBLE,
+                MAIN_PID, MPI_COMM_WORLD);
+
+    int i;
+    for (i = 0; i < numberOfNumbersPerProcess; i++) {
+        printf("myMatrix[%i]: %lf\n", i, *(*myMatrix + i));
+    }
 }
 
 int main(int argc, char **argv) {
@@ -54,13 +111,49 @@ int main(int argc, char **argv) {
 
     int m;
     int n;
-    double* vector;
-    double* matrix;
+    double *vector;
+    double *matrix;
 
-    int numberOfColumns;
+    double *myMatrix;
 
-    getInput(pid, &m, &n, &vector, &matrix);
-    calculateColumnsPerProcess(numberOfProcesses, n, &numberOfColumns);
+    int numberOfColumnsPerProcess;
+    int numberOfNumbersPerProcess;
+
+    MPI_Datatype paramsType;
+    MPI_Datatype vectorType;
+
+    createVectorType(m, n, &vectorType);
+    createParamsType(&paramsType);
+
+    if (pid == MAIN_PID) {
+        getInput(&m, &n, &vector, &matrix);
+
+//        int i;
+//        for (i = 0; i < m * n; i++) {
+//            printf("matrix[%i]: %lf\n", i, matrix[i]);
+//        }
+    }
+
+    broadcastParams(paramsType, &m, &n);
+
+//    printf("pid: %i, n: %i, m: %i\n", pid, n, m);
+
+    calculateColumnsPerProcess(numberOfProcesses, n, &numberOfColumnsPerProcess);
+    calculateNumberOfNumbersPerProcess(m, numberOfColumnsPerProcess, &numberOfNumbersPerProcess);
+
+    printf("pid: %i, nonpp: %i, cpp: %i\n", pid, numberOfNumbersPerProcess, numberOfColumnsPerProcess);
+
+    broadcastVector(pid, m, &vector);
+    broadcastMatrix(vectorType, numberOfNumbersPerProcess, numberOfColumnsPerProcess, matrix, &myMatrix);
+
+//    for (i = 0; i < m; i++) {
+//        printf("vector[%i]: %lf\n", i, vector[i]);
+//    }
+
+    int i;
+    for (i = 0; i < numberOfNumbersPerProcess; i++) {
+        printf("myMatrix[%i]: %lf\n", i, myMatrix[i]);
+    }
 
     MPI_Finalize();
 }
